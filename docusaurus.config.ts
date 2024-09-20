38fe441c050fd2a853bc6fb9ef0b0e795f4c534b
David Tang <dev@blueur.com>
2024-09-20 09:45:54 +0200

    feat: add sidebar hideable

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,6 +78,11 @@ const config: Config = {
     footer: {
       copyright: `<div xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://hep.davidtang.ch/" target="_blank">BS21INF5</a> © 2024 by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://davidtang.ch" target="_blank">David Tang</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:16px!important;margin-left:2px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"><img style="height:16px!important;margin-left:2px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg"><img style="height:16px!important;margin-left:2px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg"><img style="height:16px!important;margin-left:2px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"></a></div>`,
     },
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
     mermaid: {
       theme: { light: "base", dark: "dark" },
     },
